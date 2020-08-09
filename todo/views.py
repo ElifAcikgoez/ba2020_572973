1f14be803bf5739b66da3c5a0446ebba28ef9450
ElifAcikgoez <elifnuracikgoez@gmail.com>
2020-08-09 23:34:24 +0200

    save

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,7 @@
 
-from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .models import *
 from .forms import *
 from .models import  Task
 
