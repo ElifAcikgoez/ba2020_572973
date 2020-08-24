8cbc4406817546c267abeaa603ac6e525f8e4162
ElifAcikgoez <elifnuracikgoez@gmail.com>
2020-08-24 02:32:14 +0200

    login

--- todo/views.py
+++ todo/views.py
@@ -2,8 +2,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import *
 from .models import  Task
-
-
+from django.contrib.auth.decorators import login_required
+@login_required
 def index(request):
 	tasks = Task.objects.all()
 
